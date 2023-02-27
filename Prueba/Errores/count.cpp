b97181d386f9ae035390e5cece902a01726bdce2
AlbaMarq <101503915+AlbaMarq@users.noreply.github.com>
2023-02-27 20:08:15 +0100

    Update count.cpp

--- Prueba/Errores/count.cpp
+++ Prueba/Errores/count.cpp
@@ -1,4 +1,4 @@
-// This program periodically generates log messages at various severity levels
+// This program generates log messages at various severity levels
 
 #include<ros/ros.h>
 
@@ -8,16 +8,40 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "count_and_log");
   ros::NodeHandle nh;
 
-  // Generate log messages of varying severity regularly
-  ros::Rate rate(10);
+//SELECCIONAR SOLO UNA OPCIÓN, COMENTANDO LAS OTTRAS DOS
+
+  // 1) Generate periodically log messages of varying severity
   
-  for(int i=1; ros::ok(); i++) 
-  {
-    ROS_DEBUG_STREAM( "Counted to " << i);
-    if((i%3)==0){ ROS_INFO_STREAM(i << " is divisible by 3."); }
-    if((i%5)==0){ ROS_WARN_STREAM(i << " is divisible by 5."); }
-    if((i%10)==0){ ROS_ERROR_STREAM(i << " is divisible by 10."); }
-    if((i%20)==0){ ROS_FATAL_STREAM(i << " is divisible by 20."); }
-    rate.sleep();
-  }
+    ros::Rate rate(10);
+    for(int i=1; ros::ok(); i++) 
+    {
+      ROS_DEBUG_STREAM("Counted to " << i);
+      if((i%3)==0){ ROS_INFO_STREAM(i << " is divisible by 3."); }
+      if((i%5)==0){ ROS_WARN_STREAM(i << " is divisible by 5."); }
+      if((i%10)==0){ ROS_ERROR_STREAM(i << " is divisible by 10."); }
+      if((i%20)==0){ ROS_FATAL_STREAM(i << " is divisible by 20."); }
+      rate.sleep();
+    }
+  
+  // 2) Generates a single log message at each severity level
+  
+    while(ros::ok())
+    {
+      ROS_DEBUG_STREAM_ONCE("This appears only once.");
+      ROS_INFO_STREAM_ONCE("This appears only once.");
+      ROS_WARN_STREAM_ONCE("This appears only once.");
+      ROS_ERROR_STREAM_ONCE("This appears only once.");
+      ROS_FATAL_STREAM_ONCE("This appears only once.");
+    }
+  
+  // 3) Generates a single log message at each severity level
+  
+    while(ros::ok())
+    {
+      ROS_DEBUG_STREAM_THROTTLE(0.1, "This appears every 0,1 seconds.");
+      ROS_INFO_STREAM_THROTTLE(0.3, "This appears every 0,3 seconds.");
+      ROS_WARN_STREAM_THROTTLE(0.5, "This appears every 0,5 seconds.");
+      ROS_ERROR_STREAM_THROTTLE(1, "This appears every 1 seconds.");
+      ROS_FATAL_STREAM_THROTTLE(2, "This appears every 2 seconds.");
+    }
 }
